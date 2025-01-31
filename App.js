0877ef0a0503931781edd321d363003b06ed7d4e
Diego Quiroga <diego.quiroga1503@gmail.com>
2025-01-31 12:28:26 -0300

    prototipo

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import {View,Text, StyleSheet} from 'react-native'
+import { StatusBar } from 'react-native'
+import { colors } from './src/globals/colors'
+import { useFonts } from 'expo-font'
+import { fonts } from './src/globals/fonts'
+import Navigator from './src/navigation/Navigator'
+import { Provider } from 'react-redux'
+import {store} from './src/store'
+import { useEffect } from 'react'
+//import dotenv from 'dotenv'
 
 
-const App = () => {
+export default function App() {
 
+
+  //dotenv.config()
+  const [fontsLoaded] = useFonts(fonts)
+
+  if(!fontsLoaded){
+    return null
+  }
 
   return (
-    <View>
-      <Text>
-        Hola puto
-      </Text>
-    </View>
+    <>
+      <Provider store={store}>
+        <Navigator/>
+      </Provider>
+    
+      <StatusBar style="light" backgroundColor={colors.accent}/>
+    </>
   )
 }
 
-export default App
-
-const styles = StyleSheet.create({
-  
-})
